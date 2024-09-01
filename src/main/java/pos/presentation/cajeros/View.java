1fb72fb22be803f33ae2910927dbe40379cb1b5a
dnunezf <david.a.nunez27@outlook.com>
2024-08-31 20:51:48 -0600

    Cambio

--- src/main/java/pos/presentation/cajeros/View.java
+++ src/main/java/pos/presentation/cajeros/View.java
@@ -4,4 +4,20 @@ import javax.swing.*;
 
 public class View {
     private JPanel panel;
+    private JPanel busquedaPanel;
+    private JLabel busquedaNombreLabel;
+    private JTextField busquedaNombreTxt;
+    private JButton report;
+    private JButton search;
+    private JPanel listadoPanel;
+    private JScrollPane listadoScrollPanel;
+    private JTable list;
+    private JPanel datosClientePanel;
+    private JLabel idLabel;
+    private JTextField idTxt;
+    private JLabel nombreLabel;
+    private JTextField nombreTxt;
+    private JButton save;
+    private JButton delete;
+    private JButton clear;
 }
