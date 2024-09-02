1aa6d654e7882398ff50cf0d4bac42820a8878e1
Deivix99 <jdavid2604a@gmail.com>
2024-09-02 16:03:51 -0600

    Interfaz Producto casi lista, falta meterle el Table Model

---
+++ src/main/java/pos/presentation/productos/View.java
@@ -0,0 +1,19 @@
+package pos.presentation.productos;
+
+import javax.swing.*;
+
+public class View {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JComboBox comboBox1;
+    private JButton guardarButton;
+    private JButton limpiarButton;
+    private JButton borrarButton;
+    private JTextField textField5;
+    private JButton buscarButton;
+    private JButton reporteButton;
+    private JTable table1;
+}
