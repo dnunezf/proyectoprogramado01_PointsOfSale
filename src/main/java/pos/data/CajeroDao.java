1e8bcc45d68b227e6502c77979347d6f47dcfbb8
Andito15 <alejandro.alvarez.chaves@est.una.ac.cr>
2024-10-03 14:06:38 -0600

    CajeroDao

---
+++ src/main/java/pos/data/CajeroDao.java
@@ -0,0 +1,97 @@
+package pos.data;
+
+import pos.logic.Cajero;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CajeroDao {
+
+    Database db;
+
+    public CajeroDao() {
+        db = Database.instance();
+    }
+
+    public void create(Cajero e) throws Exception {
+        String sql = "insert into " +
+                "Cajero " +
+                "(id , nombre) " +
+                "values(?,?)";
+        PreparedStatement stm = db.prepareStatement(sql);
+        stm.setString(1, e.getId());
+        stm.setString(2, e.getNombre());
+        db.executeUpdate(stm);
+    }
+
+    public Cajero read(String id) throws Exception {
+        String sql = "select " +
+                "* " +
+                "from  Cajero t " +
+                "where t.id=?";
+        PreparedStatement stm = db.prepareStatement(sql);
+        stm.setString(1, id);
+        ResultSet rs = db.executeQuery(stm);
+
+        if (rs.next()) {
+
+            Cajero r = from(rs, "t");
+
+            return r;
+        } else {
+            throw new Exception("Cajero NO EXISTE");
+        }
+    }
+
+    public void update(Cajero e) throws Exception {
+        String sql = "update " +
+                "Cajero " +
+                "set nombre=?" +
+                "where id=?";
+        PreparedStatement stm = db.prepareStatement(sql);
+        stm.setString(1, e.getNombre());
+        stm.setString(2, e.getId());
+        int count = db.executeUpdate(stm);
+        if (count == 0) {
+            throw new Exception("Cajero NO EXISTE");
+        }
+
+    }
+
+    public void delete(Cajero e) throws Exception {
+        String sql = "delete " +
+                "from Cajero " +
+                "where id=?";
+        PreparedStatement stm = db.prepareStatement(sql);
+        stm.setString(1, e.getId());
+        int count = db.executeUpdate(stm);
+        if (count == 0) {
+            throw new Exception("Cajero NO EXISTE");
+        }
+    }
+
+    public List<Cajero> search(Cajero e) throws Exception {
+        List<Cajero> resultado = new ArrayList<Cajero>();
+        String sql = "select * " +
+                "from " +
+                "Cajero t " +
+                "where t.descripcion like ?";
+        PreparedStatement stm = db.prepareStatement(sql);
+        stm.setString(1, "%" + e.getNombre() + "%");
+        ResultSet rs = db.executeQuery(stm);
+        while (rs.next()) {
+            Cajero r = from(rs, "t");
+            resultado.add(r);
+        }
+        return resultado;
+    }
+
+    public Cajero from(ResultSet rs, String alias) throws Exception {
+        Cajero e = new Cajero();
+        e.setId(rs.getString(alias + ".id"));
+        e.setNombre(rs.getString(alias + ".nombre"));
+        return e;
+    }
+}
